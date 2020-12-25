a83419b57a25ea18299274d6f45d4477861223ac
DankeZik <19938775+ArsDankeZik@users.noreply.github.com>
2020-12-25 18:19:34 +0100

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,6 @@
+/* Function to get acestream link from each tv channel in https://m.tvron.net/ */
+
+function getLink(){
+    f = document.getElementById('mbsc');
+    console.log(f.firstElementChild.href);
+}
